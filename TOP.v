a0b935efbe0ef530a9fc8746722db358f6b20b06
keven-deoliveira <60163722+keven-deoliveira@users.noreply.github.com>
2020-12-06 18:14:52 -0500

    Added module for RGB LED output

--- TOP.v
+++ TOP.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 12/05/2020 06:37:01 AM
+// Create Date: 12/05/2020 04:20:57 PM
 // Design Name: 
 // Module Name: TOP
 // Project Name: 
@@ -24,11 +24,14 @@ module TOP(
 input clk,
 input reset,
 input [6:0] SW,
-output [1:0] WINNER       //ADD ANOTHER OUTPUT FOR VGA DISPLAY (VGA CONTROLLER) 
-    );
+//output [1:0] WINNER       //ADD ANOTHER OUTPUT FOR VGA DISPLAY (VGA CONTROLLER)
+output reg [2:0] LED_out 
+);
     
     wire [8:0] P_EN;
     reg [8:0] P1_EN, P2_EN;
+    wire [1:0] WIN;
+    wire [2:0] RGB_out;
     
     
     TTT_Decoder U0 (SW[5:2], SW[6], P_EN);
@@ -40,7 +43,7 @@ output [1:0] WINNER       //ADD ANOTHER OUTPUT FOR VGA DISPLAY (VGA CONTROLLER)
     
     else if (SW[1:0] == 2'b10) begin  //IF PLAYER SWITCH[1:0] IS 10 (PLAYER 2)
     P2_EN = P_EN; 
-    P1_EN = 9;b0;
+    P1_EN = 9'b0;
     end 
         
     else begin 
@@ -65,8 +68,14 @@ output [1:0] WINNER       //ADD ANOTHER OUTPUT FOR VGA DISPLAY (VGA CONTROLLER)
     
     fsm_controller U4(clk, reset, SW[0], SW[1], ill_move, no_space, win, P1_play, P2_play);
     
-    detect_win U5(pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, win, WINNER);
+    detect_win U5(pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, win, WIN);
     
+    rgbLED(clk, WIN, RGB_out);
+    
+    always @ (posedge clk)
+    begin
+    LED_out <= RGB_out;
+    end
     
     
     
