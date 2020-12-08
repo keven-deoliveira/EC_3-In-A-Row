252489d88ea87c6adf3f592cef4b1dd501cf2fa2
keven-deoliveira <60163722+keven-deoliveira@users.noreply.github.com>
2020-12-07 19:09:39 -0500

    Update TTT_Decoder.v

--- Design Sources/TTT_Decoder.v
+++ Design Sources/TTT_Decoder.v
@@ -23,27 +23,25 @@
 module TTT_Decoder(
     input [3:0] POS_SW,
     input ENABLE,
-    output reg [8:0] P_EN
+    output wire [8:0] P_EN
     );
+    reg [8:0] temp;
+    assign P_EN = (ENABLE == 1'b1) ? temp : 9'b0;
     
     always@ (*) begin
-    if(ENABLE)         //ENABLE IS ON
-    begin
     case(POS_SW)
-    4'd0: P_EN = 9'b000000001;
-    4'd1: P_EN = 9'b000000010;
-    4'd2: P_EN = 9'b000000100;
-    4'd3: P_EN = 9'b000001000;
-    4'd4: P_EN = 9'b000010000;
-    4'd5: P_EN = 9'b000100000;
-    4'd6: P_EN = 9'b001000000;
-    4'd7: P_EN = 9'b010000000;
-    4'd8: P_EN = 9'b100000000;
-        
-    default: P_EN = 9'b000000000;    //ANYTHING ELSE WILL NOT BE ASSIGNED TO ANY CELL
+    4'd1: temp <= 9'b000000001; // 0001
+    4'd2: temp <= 9'b000000010; // 0010
+    4'd3: temp <= 9'b000000100; // 0011
+    4'd4: temp <= 9'b000001000; // 0100
+    4'd5: temp <= 9'b000010000; // 0101
+    4'd6: temp <= 9'b000100000; // 0110
+    4'd7: temp <= 9'b001000000; // 0111
+    4'd8: temp <= 9'b010000000; // 1000
+    4'd9: temp <= 9'b100000000; // 1001
+    default: temp <= 9'b000000000;    //ANYTHING ELSE WILL NOT BE ASSIGNED TO ANY CELL
     endcase 
-    end
-    else P_EN = 9'b000000000;    //IF ENABLE IS OFF 
+  
     end 
         
 endmodule
